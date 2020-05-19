f74f706100d1bef9899262514367983bf9ae2d14
Bree Warren <ambreea.warren@gmail.com>
2020-05-19 02:54:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -5,11 +5,11 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  [{ name:  "Don Gately", occupation: "Live-in Staff" },
-  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-  { name:  "Pat Monteseian", occupation: "Staff" },
-  { name:  "Kate Gompert", occupation: "None" },
-  { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  [{DON_G},
+  {JOELLE_VD},
+  {PAT_M},
+  {KATE_G},
+  {BRUCE_G}
     ]
 end
 
