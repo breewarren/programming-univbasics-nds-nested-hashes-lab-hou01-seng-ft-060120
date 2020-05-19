e2cce355f509f550bc871a1e66bb9c9a16f7e208
Bree Warren <ambreea.warren@gmail.com>
2020-05-19 02:59:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -29,7 +29,7 @@ end
 
 def aoh_update(aoh, row, key, new_value)
   aoh[row][key] = new_value
-  aoh_update
 end
+aoh_update
  # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
